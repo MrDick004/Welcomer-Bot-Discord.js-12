35ea03bb7c71c021362d17f776abd2c337532d49
Mirko <87878460+MrDick004@users.noreply.github.com>
2023-04-11 19:31:38 +0200

    Create index.js
    Codice Index Bot

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const welcomeChannelId = 'INSERISCI L\'ID DEL CANALE DEL MESSAGGIO DI BENVENUTO';
+const goodbyeChannelId = 'INSERISCI L\'ID DEL CANALE DEL MESSAGGIO DI ADDIO';
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('guildMemberAdd', member => {
+    const welcomeChannel = member.guild.channels.cache.get(welcomeChannelId);
+    if (welcomeChannel) {
+        welcomeChannel.send(`Benvenuto/a ${member.user.tag} nel nostro server!`);
+    }
+});
+
+client.on('guildMemberRemove', member => {
+    const goodbyeChannel = member.guild.channels.cache.get(goodbyeChannelId);
+    if (goodbyeChannel) {
+        goodbyeChannel.send(`Arrivederci ${member.user.tag}! Speriamo di rivederti presto.`);
+    }
+});
+
+client.login('INSERISCI IL TOKEN DEL TUO BOT QUI');
